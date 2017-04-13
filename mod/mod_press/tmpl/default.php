072975f3aeeafd4e0c9d95efa1c8c6f446e1a38a
Unknown <zigfir@gmail.com>
2017-04-13 14:49:06 +0200

    Poprawka w module do wysiwetlania ostatnich numerow

--- mod/mod_press/tmpl/default.php
+++ mod/mod_press/tmpl/default.php
@@ -4,6 +4,8 @@ defined('_JEXEC') or die('Restricted access'); ?>
 
 <div>
 <?php
+	$kol = $kol1;
+	$dziel = (100 / $kol)-1;
 	global $check1, $check2;
 	$check1 = 0;
 	$check2 = 0;
@@ -12,15 +14,15 @@ defined('_JEXEC') or die('Restricted access'); ?>
 	$r = date('Y');
 	$rc = substr($r, 2, 2);
 			
-	function wysDC($dirName,$rc,$ost) {
+	function wysDC($dirName,$rc,$ost,$dziel) {
+			
 			global $check1, $check2;
 			for ($pl=$ost; $pl>0; $pl--) {
 				$plik = 'gazeta_'.$pl.'_'.$rc.'.jpg';
 				$plik_pdf = 'gazeta_'.$pl.'_'.$rc.'.pdf';
 				$spr = file_exists($dirName.'/'.$plik);
 				$spr_pdf = file_exists($dirName.'/'.$plik_pdf);
-				if ($spr && $spr_pdf) { 
-					echo '<div class="lastnr"><a onClick="_gaq.push([\'_trackEvent\', \'Gazeta\', \'Pliki\', \''.$plik_pdf.'\']);" href="'.$dirName.'/'.$plik_pdf.'">';
+				if ($spr && $spr_pdf) {echo '<div class="lastnr" style="width:'.$dziel.'%"><a onClick="_gaq.push([\'_trackEvent\', \'Gazeta\', \'Pliki\', \''.$plik_pdf.'\']);" href="'.$dirName.'/'.$plik_pdf.'">';
 					echo '<img src="'.$dirName.'/'.$plik.'"/></a></div>';
 					$check1++;
 					$check2 = $pl;
@@ -32,36 +34,37 @@ defined('_JEXEC') or die('Restricted access'); ?>
 		
 	
 	}
-	
-	wysDC($folder,$rc,15);
+	wysDC($folder,$rc,15,$dziel);
 	if ($check1 == 0) {
 		$rc--;
-		wysDC($folder,$rc,$ost);
+		wysDC($folder,$rc,$ost,$dziel);
 		}
 	
+	if ($kol>1) {
 	
-	
-	if ($check2==1) {
-		$rc--;
-	wysDC($folder,$rc,15);
-	}
-	else {
+		if ($check2==1) {
+			$rc--;
+			wysDC($folder,$rc,15,$dziel);
+			}
+		else {
 		$check2=$check2-1;
-		wysDC($folder,$rc,$check2);
+		wysDC($folder,$rc,$check2,$dziel);
 	}
 		
-	
-	if ($check1==2) {
+	if ($kol>2) {
+		if ($check1==2) {
 		if ($check2==1) {
 		$rc--;
-		wysDC($folder,$rc,15);
+		wysDC($folder,$rc,15,$dziel);
 		}
 		else {
 		$check2=$check2-1;
-		wysDC($folder,$rc,$check2);
+		wysDC($folder,$rc,$check2,$dziel);
+		}
+		}
+	
 	}
 	}
-		
 			
 
     
